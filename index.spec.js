7acc22cc958a022bea586e2e0e099d36b9bed9db
lrinaudo <leandro.rinaudo@unq.edu.ar>
2019-04-09 16:42:07 -0300

    Golondrinas inmutables

--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,4 @@
-import { Golondrina } from "./index"
+import { Golondrina, GolondrinasInmutables as GI} from "./index"
 
 describe("pepita basica", () => {
     it("deberia volar y consumir energia", () => {
@@ -11,5 +11,15 @@ describe("pepita basica", () => {
         const pepita = new Golondrina(30)
         pepita.comer(3)
         expect(pepita.energia).toEqual(36)
+    })
+})
+
+describe("pepita inmutable", () => {
+    it("deberia volar y consumir energia", () => {
+        const pepita = GI.volar(GI.crear("pepita"), 2)
+        expect(pepita).toMatchObject({nombre: "pepita", edad: 1, energia: 24})
+        // expect(pepita.energia).toEqual(24)
+        // expect(pepita.nombre).toEqual("pepita")
+        // expect(pepita.edad).toEqual(1)
     })
 })
